62b54484c1f594f1e55dcaccefd526e963c71159
Christien Ayson <chrisianneayson@gmail.com>
2018-10-26 19:03:02 -0700

    Transition Manager fixes and additions
    Fixed FadeOut scaling time incorrectly
    Now resets alpha of screenFadeMask before fading out in FadeScreenOut
    Added FadeScreenIn

--- HueySoftPizza/Assets/Scripts/LevelTransition.cs
+++ HueySoftPizza/Assets/Scripts/LevelTransition.cs
@@ -10,24 +10,83 @@ public class LevelTransition : MonoBehaviour
   // A black mask over the screen. Alpha is interpolated to fade the screen in and out.
   GameObject screenFadeMask;
   
+
+  /// <summary>
+  /// Initializes variables
+  /// </summary>
   void Start ()
   {
     // Fetch a pointer to the fade mask
     GameObject screenFadeMask = GameObject.Find("screenFadeMask");
   }
 
+
+  /// <summary>
+  /// Fades the screen out over fadeTime
+  /// </summary>
   public void FadeScreenOut()
   {
+    // Reset the alpha of screenFadeMask
+    Color tempColorHolder = GetComponent<SpriteRenderer>().color;
+    tempColorHolder.a = 0f;
+    screenFadeMask.GetComponent<SpriteRenderer>().color = tempColorHolder;
+
+    // Fade out the screen
     StartCoroutine(FadeOut(screenFadeMask));
   }
 
+
+  /// <summary>
+  /// Enumerator used in FadeScreenOut()
+  /// </summary>
+  /// <param name="fadedObject"></param>
+  /// <returns></returns>
   IEnumerator FadeOut (GameObject fadedObject)
   {
     // Decrement alpha over time so that it reaches maxTransparency after fadeTime
-    for (float f = maxTransparency; f <= fadeTime; f += Time.deltaTime)
+    for (float f = 0; f <= fadeTime; f += Time.deltaTime)
     {
       // Find the percent to interpolate between max transparency and min transparency
       float interpolationValue = (f / fadeTime);
+
+      // Use a temporary Color object to set the new Color
+      Color tempColorHolder = fadedObject.GetComponent<SpriteRenderer>().color;
+      tempColorHolder.a = Mathf.Lerp(minTransparency, maxTransparency, interpolationValue);
+      fadedObject.GetComponent<SpriteRenderer>().color = tempColorHolder;
+
+      // Advance to next frame
+      yield return null;
+    }
+  }
+
+
+  /// <summary>
+  /// Fades the screen in over fadeTime
+  /// </summary>
+  public void FadeScreenIn()
+  {
+    // Reset the alpha of screenFadeMask
+    Color tempColorHolder = GetComponent<SpriteRenderer>().color;
+    tempColorHolder.a = 1f;
+    screenFadeMask.GetComponent<SpriteRenderer>().color = tempColorHolder;
+
+    // Fade out the screen
+    StartCoroutine(FadeIn(screenFadeMask));
+  }
+
+
+  /// <summary>
+  /// Enumerator used in FadeScreenIn()
+  /// </summary>
+  /// <param name="fadedObject"></param>
+  /// <returns></returns>
+  IEnumerator FadeIn(GameObject fadedObject)
+  {
+    // Decrement alpha over time so that it reaches maxTransparency after fadeTime
+    for (float f = 0; f <= fadeTime; f += Time.deltaTime)
+    {
+      // Find the percent to interpolate between max transparency and min transparency
+      float interpolationValue = (1 - (f / fadeTime));
 
       // Use a temporary Color object to set the new Color
       Color tempColorHolder = fadedObject.GetComponent<SpriteRenderer>().color;
