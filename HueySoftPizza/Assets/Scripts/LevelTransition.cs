3758b42fc13cd1307eb7e2720decd830757e5801
Christien Ayson <chrisianneayson@gmail.com>
2018-10-26 18:40:51 -0700

    Transition Manager - Screen Fade Out

---
+++ HueySoftPizza/Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine; // MonoBehavior
+
+public class LevelTransition : MonoBehaviour
+{
+  // Fade Parameters
+  const float fadeTime = 1.0f;
+  const float minTransparency = 1.0f; // 1.0 = completely visible
+  const float maxTransparency = 0.0f; // 0.0 = completely invisible
+
+  // A black mask over the screen. Alpha is interpolated to fade the screen in and out.
+  GameObject screenFadeMask;
+  
+  void Start ()
+  {
+    // Fetch a pointer to the fade mask
+    GameObject screenFadeMask = GameObject.Find("screenFadeMask");
+  }
+
+  public void FadeScreenOut()
+  {
+    StartCoroutine(FadeOut(screenFadeMask));
+  }
+
+  IEnumerator FadeOut (GameObject fadedObject)
+  {
+    // Decrement alpha over time so that it reaches maxTransparency after fadeTime
+    for (float f = maxTransparency; f <= fadeTime; f += Time.deltaTime)
+    {
+      // Find the percent to interpolate between max transparency and min transparency
+      float interpolationValue = (f / fadeTime);
+
+      // Use a temporary Color object to set the new Color
+      Color tempColorHolder = fadedObject.GetComponent<SpriteRenderer>().color;
+      tempColorHolder.a = Mathf.Lerp(minTransparency, maxTransparency, interpolationValue);
+      fadedObject.GetComponent<SpriteRenderer>().color = tempColorHolder;
+
+      // Advance to next frame
+      yield return null;
+    }
+  }
+}
