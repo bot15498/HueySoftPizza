e21909da9886fa5bd6cb150d78afedab74a511a3
skyler <konata.izumi20@gmail.com>
2018-10-27 06:04:30 -0400

    added about me text.

--- HueySoftPizza/Assets/Scripts/Button/Button.cs
+++ HueySoftPizza/Assets/Scripts/Button/Button.cs
@@ -40,16 +40,25 @@ public class Button : MonoBehaviour
     public Text SexAgeField;
     public Text BioField;
     public Text FeedText1;
-    public Text FeedText2;
 
+    [SerializeField]
     private string currName;
+    [SerializeField]
     private Sex currSex;
+    [SerializeField]
     private Age currAge;
+    [SerializeField]
     private Hobbies currHobby;
+    [SerializeField]
     private Education currEd;
 
     private List<string> firstNames = new List<string>();
     private List<string> lastNames = new List<string>();
+    private List<string> hobbyAnime = new List<string>();
+    private List<string> hobbyFortnite = new List<string>();
+    private List<string> hobbyDabbing = new List<string>();
+    private List<string> edHighSchool = new List<string>();
+    private List<string> edCollege = new List<string>();
 
 
     // Use this for initialization
@@ -70,6 +79,41 @@ public class Button : MonoBehaviour
             lastNames.Add(line);
         }
         reader.Close();
+        //About me, hobby, anime
+        reader = new StreamReader("Assets/Stories/AboutMes/Hobbies/Anime.txt");
+        while ((line = reader.ReadLine()) != null)
+        {
+            hobbyAnime.Add(line);
+        }
+        reader.Close();
+        //About me, hobby, fortnite
+        reader = new StreamReader("Assets/Stories/AboutMes/Hobbies/Fortnite.txt");
+        while ((line = reader.ReadLine()) != null)
+        {
+            hobbyFortnite.Add(line);
+        }
+        reader.Close();
+        //About me, hobby, fortnite
+        reader = new StreamReader("Assets/Stories/AboutMes/Hobbies/Dabbing.txt");
+        while ((line = reader.ReadLine()) != null)
+        {
+            hobbyDabbing.Add(line);
+        }
+        reader.Close();
+        //About me, education, high school
+        reader = new StreamReader("Assets/Stories/AboutMes/Education/HighSchool.txt");
+        while ((line = reader.ReadLine()) != null)
+        {
+            edHighSchool.Add(line);
+        }
+        reader.Close();
+        //About me, education, college
+        reader = new StreamReader("Assets/Stories/AboutMes/Education/College.txt");
+        while ((line = reader.ReadLine()) != null)
+        {
+            edCollege.Add(line);
+        }
+        reader.Close();
         ShowNewPerson();
     }
 
@@ -85,6 +129,8 @@ public class Button : MonoBehaviour
         currName = firstNames[Random.Range(0, firstNames.Count - 1)] + " " + lastNames[Random.Range(0, lastNames.Count - 1)];
         currAge = (Age)Random.Range(0, System.Enum.GetNames(typeof(Age)).Length - 1);
         currSex = (Sex)Random.Range(0, 2);
+        currHobby = (Hobbies)Random.Range(0, System.Enum.GetNames(typeof(Hobbies)).Length - 1);
+        currEd = (Education)Random.Range(0, System.Enum.GetNames(typeof(Education)).Length - 1);
 
         //Update text on screen
         NameField.text = currName;
@@ -92,5 +138,23 @@ public class Button : MonoBehaviour
             : currAge == Age.Adult ? Random.Range(26, 49)
             : Random.Range(50, 98);
         SexAgeField.text = currSex + ", " + actualAge;
+        if(Random.Range(0,1) == 0)
+        {
+            FeedText1.text = currHobby == Hobbies.Anime ? hobbyAnime[Random.Range(0, hobbyAnime.Count - 1)]
+                : currHobby == Hobbies.Fortnite ? hobbyFortnite[Random.Range(0, hobbyFortnite.Count - 1)]
+                : hobbyDabbing[Random.Range(0, hobbyDabbing.Count - 1)];
+            FeedText1.text += "\n\n";
+            FeedText1.text += currEd == Education.HighSchool ? edHighSchool[Random.Range(0, edHighSchool.Count - 1)]
+                : edCollege[Random.Range(0, edCollege.Count - 1)];
+        }
+        else
+        {
+            FeedText1.text = currEd == Education.HighSchool ? edHighSchool[Random.Range(0, edHighSchool.Count - 1)]
+                : edCollege[Random.Range(0, edCollege.Count - 1)];
+            FeedText1.text += "\n\n";
+            FeedText1.text += currHobby == Hobbies.Anime ? hobbyAnime[Random.Range(0, hobbyAnime.Count - 1)]
+                : currHobby == Hobbies.Fortnite ? hobbyFortnite[Random.Range(0, hobbyFortnite.Count - 1)]
+                : hobbyDabbing[Random.Range(0, hobbyDabbing.Count - 1)];
+        }
     }
 }
