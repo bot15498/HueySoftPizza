5818f290ab4ced58189fd986926415b01cc12b5e
skyler <konata.izumi20@gmail.com>
2018-10-28 03:07:39 -0400

    added flavor text for bad endings

--- HueySoftPizza/Assets/Scripts/BadEnd.cs
+++ HueySoftPizza/Assets/Scripts/BadEnd.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,8 +9,11 @@ public class BadEnd : MonoBehaviour
 {
   public Text TitleField;
   public Text DescField;
+  public List<string> Titles;
+  public List<string> Descriptions;
 
   private LevelTransition transitionManager;
+  private PlayerInfo playerInfo;
 
   void Start()
   {
@@ -16,6 +21,9 @@ public class BadEnd : MonoBehaviour
     {
       transitionManager = FindObjectOfType<LevelTransition>();
     }
+    Titles = new List<string>();
+    Descriptions = new List<string>();
+    LoadEndGameText();
   }
 
   void Update()
@@ -24,10 +32,42 @@ public class BadEnd : MonoBehaviour
     {
       transitionManager = FindObjectOfType<LevelTransition>();
     }
+    if (playerInfo == null)
+    {
+      playerInfo = FindObjectOfType<PlayerInfo>();
+      UpdateText(playerInfo.currState);
+    }
   }
 
   public void BackToMainMenu()
   {
     StartCoroutine(transitionManager.TransitionScene("MainMenu"));
+  }
+
+  private void UpdateText(EndStates state)
+  {
+    TitleField.text = Titles[(int)state];
+    DescField.text = Descriptions[(int)state];
+  }
+
+  private void LoadEndGameText()
+  {
+    string line;
+    Titles.Add("Dummy None Placeholder");
+    //Titles
+    StreamReader reader = new StreamReader("Assets/Stories/BadEndTitles.txt", Encoding.GetEncoding(1252));
+    while ((line = reader.ReadLine()) != null)
+    {
+      Titles.Add(line);
+    }
+    reader.Close();
+    //Descriptions
+    Descriptions.Add("Dummy None Placeholder");
+    reader = new StreamReader("Assets/Stories/BadEndDesc.txt", Encoding.GetEncoding(1252));
+    while ((line = reader.ReadLine()) != null)
+    {
+      Descriptions.Add(line);
+    }
+    reader.Close();
   }
 }
